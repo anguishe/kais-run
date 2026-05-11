23768a64c06aadb902250c047cefa9de74e9986f
anguishe <anguisheh1@gmail.com>
2026-05-11 15:45:18 -0500

    kais run CNAME

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-    ],
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/kais-run' : '',
+  images: { unoptimized: true },
   trailingSlash: true,
 };
 module.exports = nextConfig;
